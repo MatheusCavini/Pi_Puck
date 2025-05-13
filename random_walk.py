f0df7cfca62cfd54096c8fa69092b27587487f16
Matheus Latorre Cavini <matheuslcavini@usp.br>
2025-05-13 07:13:12 -0300

    fast blink

--- random_walk.py
+++ random_walk.py
@@ -28,9 +28,12 @@ def on_message(client, userdata, message):
     try:
         if message.topic == f"robot/{MY_ID}":
             print(f"Message received on {message.topic}: {message.payload.decode()}")
-            pipuck.set_leds_colour("cyan")
-            time.sleep(0.3)
-            pipuck.set_leds_colour("off")
+            for i in range(3):
+                pipuck.set_leds_colour("cyan")
+                time.sleep(0.05)
+                pipuck.set_leds_colour("off")
+                time.sleep(0.05)
+            
         elif message.topic == "robot_pos/all":
             data = json.loads(message.payload.decode())
             msg = data
