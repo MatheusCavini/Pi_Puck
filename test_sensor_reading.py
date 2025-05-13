81dbcc271c615dc330212af24e42186c6e314b73
Matheus Latorre Cavini <matheuslcavini@usp.br>
2025-05-13 06:15:27 -0300

    fix bug

--- test_sensor_reading.py
+++ test_sensor_reading.py
@@ -43,7 +43,7 @@ while True:
     if(checksum == sensors_data[SENSORS_SIZE-1]):
         for i in range(8):
             prox[i] = sensors_data[i*2+1]*256+sensors_data[i*2]
-            print("prox: {0:4d}, {1:4d}, {2:4d}, {3:4d}, {4:4d}, {5:4d}, {6:4d}, {7:4d}\r\n".format(prox[0], prox[1], prox[2], prox[3], prox[4], prox[5], prox[6], prox[7]))
+        print("prox: {0:4d}, {1:4d}, {2:4d}, {3:4d}, {4:4d}, {5:4d}, {6:4d}, {7:4d}\r\n".format(prox[0], prox[1], prox[2], prox[3], prox[4], prox[5], prox[6], prox[7]))
     else:
         print("Checksum error")
         sys.exit(1)
